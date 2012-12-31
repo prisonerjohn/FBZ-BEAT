2327913459cc5f44d05f5a1d78f431dac15cd950
Elie Zananiri <prisonerjohn@gmail.com>
2012-12-31 14:42:51 -0500

    Updated logic to use the main application loop to iterate through data. This allows us to read the tempo from the potentiometer at any time.

--- FBZ_BEAT.ino
+++ FBZ_BEAT.ino
@@ -7,42 +7,38 @@ int dataPin = 5;
 
 int pot = A0;
 int tempo;
+int numberToDisplay;
 
 void setup() {
   //set pins to output so you can control the shift register
   pinMode(latchPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
   pinMode(dataPin, OUTPUT);
+  
+  numberToDisplay = 1;
 }
 
 void loop() {
   Serial.begin(9600);
   tempo = analogRead(pot);
-  // count from 0 to 255 and display the number 
-  // on the LEDs
-  for (int numberToDisplay = 1; numberToDisplay < 129; numberToDisplay = numberToDisplay*2) {
+  
+  if (numberToDisplay < 129) {
     // take the latchPin low so 
     // the LEDs don't change while you're sending in bits:
     digitalWrite(latchPin, LOW); 
-    // shift out the bits:
-    //shiftOut(dataPin, clockPin, MSBFIRST, 1); //LED 1
-    //shiftOut(dataPin, clockPin, MSBFIRST, 2); //LED 2
-    //shiftOut(dataPin, clockPin, MSBFIRST, 3); //LED 1 & 2
-    //shiftOut(dataPin, clockPin, MSBFIRST, 4); //LED 3
-    //shiftOut(dataPin, clockPin, MSBFIRST, 8); //LED 4
-    //shiftOut(dataPin, clockPin, MSBFIRST, 16); //LED 5
-    //shiftOut(dataPin, clockPin, MSBFIRST, 32); //LED 6
-    //shiftOut(dataPin, clockPin, MSBFIRST, 64); //LED 7
-    //shiftOut(dataPin, clockPin, MSBFIRST, 128); //LED 8
     
-      shiftOut(dataPin, clockPin, MSBFIRST, numberToDisplay); //LED 2
-      //Serial.println(numberToDisplay);
-
+    shiftOut(dataPin, clockPin, MSBFIRST, numberToDisplay);
+    
     //take the latch pin high so the LEDs will light up:
     digitalWrite(latchPin, HIGH);
+    
+    // increment the number for the next iteration:
+    numberToDisplay = numberToDisplay*2;
+    
     // pause before next value:
     delay(tempo);
- }
- 
- 
+  }
+  else {
+    numberToDisplay = 1;
+  }
 }
